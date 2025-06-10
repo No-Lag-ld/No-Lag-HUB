dced1dd91137ade71d27f69fa1db397a029eac87
NoLag-ld <uddinryan06@gmail.com>
2025-06-10 19:03:05 +0800

    Update LoaderV1.lua

--- Loader/LoaderV1.lua
+++ Loader/LoaderV1.lua
@@ -1,12 +1,12 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local scripts = {
-    [126884695634066] = "",
-    [81440632616906] = "",
+    [126884695634066] = "https://raw.githubusercontent.com/No-Lag-id/No-Lag-HUB/refs/heads/main/untitled.lua",
+    [81440632616906] = "https://raw.githubusercontent.com/No-Lag-id/No-Lag-HUB/refs/heads/main/untitled.lua",
 }
 
 local url = scripts[game.PlaceId]
 if url then
     loadstring(game:HttpGetAsync(url))()
-    loadstring(game:HttpGetAsync(""))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/No-Lag-id/No-Lag-HUB/refs/heads/main/untitled.lua"))()
 end
